09a63870dec4de8ea03f2008163beff8029fdadd
Vladyslav Demchenko <vladyslav.demchenko.99@gmail.com>
2020-01-12 23:18:35 +0000

    implements repository for auth users

---
+++ Infrastructure/DapperDataAccess/Entities/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Identity.Role;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DapperDataAccess.Entities
+{
+    public class ApplicationRole : Role
+    {
+        public string NormalizedName { get; set; }
+    }
+}
---
+++ Infrastructure/DapperDataAccess/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Identity;
+
+namespace Infrastructure.DapperDataAccess.Entities
+{
+    public class ApplicationUser : User
+    {
+        public string NormalizedUserName { get; set; }
+        public string NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+    }
+}
